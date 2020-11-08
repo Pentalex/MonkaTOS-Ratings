50b194be6774faa8751754075bf704f678978a69
Animo <13455401+ohmree@users.noreply.github.com>
2020-11-08 12:38:36 +0200

    Refactored extension to use browser polyfill and async/await
    Also some naming changes for clarity and consistency,
    and some formatting of html strings.

--- src/js/background.js
+++ src/js/background.js
@@ -1,45 +1,43 @@
 import "../img/icon128.png";
+import browser from "webextension-polyfill";
 
-chrome.runtime.onInstalled.addListener((_object) => {
-    chrome.tabs.create({ url: "/options.html" }, (_tab) => {});
+browser.runtime.onInstalled.addListener(async (_object) => {
+    await browser.tabs.create({ url: "/options.html" });
 });
 
-function refresh() {
-    chrome.storage.sync.get(["access_token"], (result) => {
-        const url = `https://twitchtos.herokuapp.com/refresh?access_token=${result.access_token}`;
-        fetch(url)
-            .then((r) => r.text())
-            .then((result) => {
-                if (result == "bruh") {
-                    chrome.storage.sync.set({ logged_in: false });
-                } else {
-                    chrome.storage.sync.set({ access_token: result });
-                    console.log(`Refreshing the thing ${result}`);
-                }
-            });
-    });
+async function refresh() {
+    const syncResult = await browser.storage.sync.get(["access_token"]);
+    const url = `https://twitchtos.herokuapp.com/refresh?access_token=${syncResult.access_token}`;
+    const response = await fetch(url);
+    const result = await response.text();
+    if (result == "bruh") {
+        await browser.storage.sync.set({ logged_in: false });
+    } else {
+        await browser.storage.sync.set({ access_token: result });
+        console.log(`Refreshing the thing ${result}`);
+    }
 }
 
 setInterval(refresh, 1000 * 500);
 
-chrome.storage.onChanged.addListener((changes, _namespace) => {
+browser.storage.onChanged.addListener(async (changes, _namespace) => {
     for (const key in changes) {
         const storageChange = changes[key];
         if (key == "authcode") {
             const authCode = storageChange.newValue;
-            fetch(`https://twitchtos.herokuapp.com/auth?authcode=${authCode}`)
-                .then((r) => r.text())
-                .then((result) => {
-                    chrome.storage.sync.set({ access_token: result });
-                    chrome.storage.sync.set({ logged_in: true });
-                    console.log("Logged in");
-                });
+            const response = await fetch(
+                `https://twitchtos.herokuapp.com/auth?authcode=${authCode}`
+            );
+            const result = await response.text();
+            await browser.storage.sync.set({ access_token: result });
+            await browser.storage.sync.set({ logged_in: true });
+            console.log("Logged in");
         }
     }
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     if (changeInfo && changeInfo.status == "complete") {
-        chrome.tabs.sendMessage(tabId, { data: tab }, () => {});
+        await browser.tabs.sendMessage(tabId, { data: tab });
     }
 });
--- src/js/content.js
+++ src/js/content.js
@@ -1,11 +1,13 @@
-/* eslint-disable no-undef */
 import $ from "jquery";
+import browser from "webextension-polyfill";
+
 import "../img/downvote.png";
 import "../img/upvote.png";
 import "../img/18px_level1.png";
 import "../img/18px_level2.png";
 import "../img/18px_level3.png";
 import "../img/18px_donator.png";
+import "../img/18px_betatester.png";
 
 console.log("Running script");
 
@@ -15,7 +17,7 @@ function youtubeParser(url) {
     return match && match[7].length == 11 ? match[7] : false;
 }
 
-chrome.runtime.onMessage.addListener((_request, _sender, _sendResponse) => {
+browser.runtime.onMessage.addListener((_request, _sender, _sendResponse) => {
     console.log("page change");
 
     setTimeout(() => {
@@ -23,19 +25,19 @@ chrome.runtime.onMessage.addListener((_request, _sender, _sendResponse) => {
     }, 500);
 });
 
-function elementLoaded(el, cb) {
+async function elementLoaded(el) {
     if ($(el).length) {
         // Element is now loaded.
-        cb($(el));
+        return $(el);
     } else {
         // Repeat every 500ms.
-        setTimeout(() => {
-            elementLoaded(el, cb);
+        setTimeout(async () => {
+            await elementLoaded(el);
         }, 500);
     }
 }
 
-function init() {
+async function init() {
     const url = window.location.href;
     const videoId = youtubeParser(url);
     console.log(videoId);
@@ -43,193 +45,182 @@ function init() {
         return;
     }
 
-    elementLoaded(".ytd-video-primary-info-renderer", (_el) => {
-        fetch(`https://twitchtos.herokuapp.com/getrating?video_id=${videoId}`)
-            .then((r) => r.text())
-            .then((result) => {
-                console.log(result);
-                let scoreText;
-                if (parseInt(result) > 0 && parseInt(result) < 3) {
-                    scoreText = "Decent";
-                } else if (parseInt(result) < 0 && parseInt(result) > -3) {
-                    scoreText = "Bad";
-                } else if (parseInt(result) <= -3) {
-                    scoreText = "Terrible";
-                } else if (parseInt(result) >= 3) {
-                    scoreText = "Good";
-                }
+    await elementLoaded(".ytd-video-primary-info-renderer");
+    const response = await fetch(
+        `https://twitchtos.herokuapp.com/getrating?video_id=${videoId}`
+    );
+    const text = await response.text();
+    console.log(text);
+    let scoreText;
+    const parsed = parseInt(text);
+    if (parsed > 0 && parsed < 3) {
+        scoreText = "Decent";
+    } else if (parsed < 0 && parsed > -3) {
+        scoreText = "Bad";
+    } else if (parsed <= -3) {
+        scoreText = "Terrible";
+    } else if (parsed >= 3) {
+        scoreText = "Good";
+    }
 
-                const el = document.getElementsByClassName(
-                    "ytd-video-primary-info-renderer"
-                )[0].children[5].children[1];
-                console.log(
-                    document.getElementsByClassName(
-                        "ytd-video-primary-info-renderer"
-                    )[0].children[5]
-                );
-                $(".toscontainer").empty();
-                $(".toscontainer").remove();
-                el.insertAdjacentHTML(
-                    "beforeend",
-                    '<div class="toscontainer"></div>'
-                );
+    const el = document.getElementsByClassName(
+        "ytd-video-primary-info-renderer"
+    )[0].children[5].children[1];
+    console.log(
+        document.getElementsByClassName("ytd-video-primary-info-renderer")[0]
+            .children[5]
+    );
+    $(".toscontainer").empty();
+    $(".toscontainer").remove();
+    el.insertAdjacentHTML("beforeend", '<div class="toscontainer"></div>');
 
-                if (result == "approved") {
-                    $(".toscontainer").append(
-                        `
+    const upvoteURL = browser.runtime.getURL("upvote.png");
+    const downvoteURL = browser.runtime.getURL("downvote.png");
+    if (text == "approved") {
+        $(".toscontainer").append(
+            `
         <img style="float: left;padding: 5px;"
-             src="${chrome.extension.getURL("upvote.png")}"
+             src="${upvoteURL}"
              width="24"
              height="24"
              id="upvote" />
         <img style="float: left;display: none;"
-             src="${chrome.extension.getURL("downvote.png")}"
+             src="${downvoteURL}"
              width="24"
              height="24"
              id="downvote" />
         <p id="tos" style="float: left;  padding 5px;">The creator of this video is guaranteed to be TOS friendly.</span></p>
         `
-                    );
-                } else if (result == "0") {
-                    $(".toscontainer").append(
-                        `
+        );
+    } else if (text == "0") {
+        $(".toscontainer").append(
+            `
         <img style="cursor: pointer; float: left;padding: 5px;"
-             src="${chrome.extension.getURL("upvote.png")}"
+             src="${upvoteURL}"
              width="24"
              height="24"
              id="upvote" />
         <img style="cursor: pointer; float: left;padding: 5px;"
-             src="${chrome.extension.getURL("downvote.png")}"
+             src="${downvoteURL}"
              width="24"
              height="24"
              id="downvote" />
         <p id="tos" style="float: left;  padding 5px;">This video hasn't been rated.</span></p>
         `
-                    );
-                } else {
-                    $(".toscontainer").empty();
-                    $(".toscontainer").append(
-                        `
+        );
+    } else {
+        $(".toscontainer").empty();
+        $(".toscontainer").append(
+            `
           <img style="cursor: pointer; float: left;padding: 5px;"
-               src="${chrome.extension.getURL("upvote.png")}"
+               src="${upvoteURL}"
                width="24"
                height="24"
                id="upvote" />
           <img style="cursor: pointer; float: left;padding: 5px;"
-               src="${chrome.extension.getURL("downvote.png")}"
+               src="${downvoteURL}"
                width="24"
                height="24"
                id="downvote" />
           <p id="tos" style="float: left;  padding 5px;">
-              TOS Score: <span id='scoretext'>${scoreText}(${result})</span>
+              TOS Score: <span id='scoretext'>${scoreText}(${text})</span>
           </p>
           `
-                    );
-                }
+        );
+    }
 
-                const upvoteButton = document.getElementById("upvote");
-                const downvoteButton = document.getElementById("downvote");
-                upvoteButton.addEventListener("click", upvote);
-                downvoteButton.addEventListener("click", downvote);
+    const upvoteButton = document.getElementById("upvote");
+    const downvoteButton = document.getElementById("downvote");
+    upvoteButton.addEventListener("click", upvote);
+    downvoteButton.addEventListener("click", downvote);
 
-                if (
-                    window.matchMedia &&
-                    window.matchMedia("(prefers-color-scheme: dark)").matches
-                ) {
-                    document.getElementById("tos").style.color = "white";
-                } else {
-                    document.getElementById("tos").style.color = "black";
-                }
-                document.getElementById("tos").style.fontSize = "medium";
+    if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        document.getElementById("tos").style.color = "white";
+    } else {
+        document.getElementById("tos").style.color = "black";
+    }
+    document.getElementById("tos").style.fontSize = "medium";
 
-                document.getElementById("tos").style.paddingTop = "7px";
+    document.getElementById("tos").style.paddingTop = "7px";
 
-                if (result == "approved") {
-                    document.getElementById("tos").style.color = "Green";
-                }
+    if (text == "approved") {
+        document.getElementById("tos").style.color = "Green";
+    }
 
-                if (scoreText == "Good") {
-                    document.getElementById("scoretext").style.color = "Green";
-                }
-                if (scoreText == "Decent") {
-                    document.getElementById("scoretext").style.color =
-                        "GreenYellow";
-                }
-                if (scoreText == "Bad") {
-                    document.getElementById("scoretext").style.color =
-                        "IndianRed";
-                }
-                if (scoreText == "Terrible") {
-                    document.getElementById("scoretext").style.color = "Red";
-                }
-            });
-    });
+    if (scoreText == "Good") {
+        document.getElementById("scoretext").style.color = "Green";
+    }
+    if (scoreText == "Decent") {
+        document.getElementById("scoretext").style.color = "GreenYellow";
+    }
+    if (scoreText == "Bad") {
+        document.getElementById("scoretext").style.color = "IndianRed";
+    }
+    if (scoreText == "Terrible") {
+        document.getElementById("scoretext").style.color = "Red";
+    }
 }
 
-function upvote() {
+async function upvote() {
     const url = window.location.href;
     const videoId = youtubeParser(url);
     const upvoteButton = document.getElementById("upvote");
     const downvoteButton = document.getElementById("downvote");
 
-    chrome.storage.sync.get(["access_token"], (result) => {
-        fetch(
-            `https://twitchtos.herokuapp.com/rate?video_id=${videoId}&rating=plus`,
-            { headers: { Authorization: result.access_token } }
-        )
-            .then((r) => r.text())
-            .then((result) => {
-                console.log(result);
-                if (result === "found") {
-                    alert("You have already upvoted this video!");
-                    return;
-                } else if (result === "Successfuly rated video!") {
-                    console.log(result);
-                    downvoteButton.parentNode.removeChild(downvoteButton);
-                    upvoteButton.style.outline = "auto";
-                    upvoteButton.style.outlineOffset = "-4px";
-                    init();
-                } else {
-                    alert(
-                        "Failed to upvote video, please try signing in again."
-                    );
-                    chrome.storage.sync.set({ logged_in: false });
-                }
-            });
-    });
+    const result = await browser.storage.sync.get(["access_token"]);
+    const response = await fetch(
+        `https://twitchtos.herokuapp.com/rate?video_id=${videoId}&rating=plus`,
+        { headers: { Authorization: result.access_token } }
+    );
+    const text = await response.text();
+    console.log(text);
+    if (text === "found") {
+        alert("You have already upvoted this video!");
+        return;
+    } else if (text === "Successfuly rated video!") {
+        console.log(text);
+        downvoteButton.parentNode.removeChild(downvoteButton);
+        upvoteButton.style.outline = "auto";
+        upvoteButton.style.outlineOffset = "-4px";
+        await init();
+    } else {
+        alert("Failed to upvote video, please try signing in again.");
+        await browser.storage.sync.set({ logged_in: false });
+    }
 }
 
-function downvote() {
+async function downvote() {
     const url = window.location.href;
     const videoId = youtubeParser(url);
     const upvoteButton = document.getElementById("upvote");
     const downvoteButton = document.getElementById("downvote");
 
-    chrome.storage.sync.get(["access_token"], (result) => {
-        fetch(
-            `https://twitchtos.herokuapp.com/rate?video_id=${videoId}&rating=minus`,
-            { headers: { Authorization: result.access_token } }
-        )
-            .then((r) => r.text())
-            .then((result) => {
-                if (result === "found") {
-                    alert("You have already downvoted this video!");
-                    return;
-                } else if (result === "Successfuly rated video!") {
-                    console.log(result);
-                    upvoteButton.parentNode.removeChild(upvoteButton);
-                    downvoteButton.style.outline = "auto";
-                    downvoteButton.style.outlineOffset = "-4px";
-                    init();
-                } else {
-                    alert(
-                        "Failed to downvote video, please try signing in again."
-                    );
-                    chrome.storage.sync.set({ logged_in: false });
-                }
-            });
-    });
+    const result = await browser.storage.sync.get(["access_token"]);
+    const response = await fetch(
+        `https://twitchtos.herokuapp.com/rate?video_id=${videoId}&rating=minus`,
+        { headers: { Authorization: result.access_token } }
+    );
+    const text = await response.text();
+    if (text === "found") {
+        alert("You have already downvoted this video!");
+        return;
+    } else if (text === "Successfuly rated video!") {
+        console.log(text);
+        upvoteButton.parentNode.removeChild(upvoteButton);
+        downvoteButton.style.outline = "auto";
+        downvoteButton.style.outlineOffset = "-4px";
+        init();
+    } else {
+        alert("Failed to downvote video, please try signing in again.");
+        await browser.storage.sync.set({ logged_in: false });
+    }
 }
+
+const donatorURL = browser.runtime.getURL("18px_donator.png");
+const betaTesterURL = browser.runtime.getURL("18px_betatester.png");
 
 //Special credits to RavenBtw
 if (window.location.href.indexOf("twitch")) {
@@ -283,9 +274,11 @@ if (window.location.href.indexOf("twitch")) {
                                         .querySelector(".chat-line__username")
                                         .insertAdjacentHTML(
                                             "beforebegin",
-                                            `<a title="TwitchTOS Donator <3" href="https://chrome.google.com/webstore/detail/monkatos-ratings/iecemifilihdioifbjkecacedfgfbfpl" target="_blank"><img src="${chrome.extension.getURL(
-                                                "18px_donator.png"
-                                            )}" class="chat-badge"></a>`
+                                            `<a title="TwitchTOS Donator <3"
+                                                               href="https://chrome.google.com/webstore/detail/monkatos-ratings/iecemifilihdioifbjkecacedfgfbfpl"
+                                                               target="_blank">
+                                                                   <img src="${donatorURL}" class="chat-badge">
+                                                            </a>`
                                         );
                                 } else if (foundUserLevel === 98) {
                                     console.log("Username found");
@@ -293,9 +286,11 @@ if (window.location.href.indexOf("twitch")) {
                                         .querySelector(".chat-line__username")
                                         .insertAdjacentHTML(
                                             "beforebegin",
-                                            `<a title="TwitchTOS Beta Tester <3" href="https://chrome.google.com/webstore/detail/monkatos-ratings/iecemifilihdioifbjkecacedfgfbfpl" target="_blank"><img src="${chrome.extension.getURL(
-                                                "18px_betatester.png"
-                                            )}" class="chat-badge"></a>`
+                                            `<a title="TwitchTOS Beta Tester <3"
+                                                               href="https://chrome.google.com/webstore/detail/monkatos-ratings/iecemifilihdioifbjkecacedfgfbfpl"
+                                                               target="_blank">
+                                                                   <img src="${betaTesterURL}" class="chat-badge">
+                                                               </a>`
                                         );
                                 } else {
                                     console.log("Username found");
@@ -303,9 +298,13 @@ if (window.location.href.indexOf("twitch")) {
                                         .querySelector(".chat-line__username")
                                         .insertAdjacentHTML(
                                             "beforebegin",
-                                            `<a title="TwitchTOS Level ${foundUserLevel}" href="https://chrome.google.com/webstore/detail/monkatos-ratings/iecemifilihdioifbjkecacedfgfbfpl" target="_blank"><img src="${chrome.extension.getURL(
-                                                `18px_level${foundUserLevel}.png`
-                                            )}" class="chat-badge"></a>`
+                                            `<a title="TwitchTOS Level ${foundUserLevel}"
+                                                               href="https://chrome.google.com/webstore/detail/monkatos-ratings/iecemifilihdioifbjkecacedfgfbfpl"
+                                                               target="_blank">
+                                                                   <img src="${browser.runtime.getURL(
+                                                                       `18px_level${foundUserLevel}.png`
+                                                                   )}" class="chat-badge">
+                                                               </a>`
                                         );
                                 }
                             }
